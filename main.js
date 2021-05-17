d4c32102d6c897adc3c4a260a1dbff428013e9e4
victorwellington02 <wellington.vlnunes02@gmail.com>
2021-05-17 08:01:48 -0300

    Desafio Finalizado

---
+++ main.js
@@ -0,0 +1,23 @@
+//Desafio 3 - número 1
+for(let contador = 1; contador <= 10; contador++){
+    console.log(contador)
+}
+
+//Desafio 3 - número 2
+let i = 10
+while(i >= 1){
+    console.log(i)
+    i--;
+}
+
+//Desafio 3 - número 3
+for(let cont = 1; cont <= 100; cont += 2){
+    console.log(cont)
+}
+
+//Desafio 3 - número 4
+let b = 0
+while(b <= 100){
+    console.log(b)
+    b+=2;
+}
